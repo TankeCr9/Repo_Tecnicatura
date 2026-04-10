383f17bfca44716e66d6bcd8def73d2866e57ebd
Esteban Craia <110699602+TankeCr9@users.noreply.github.com>
2026-04-09 23:46:02 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
 #Prueba para pullrequest
 print ("Hello, World!")
-
 nombre = input("Bienvenido, ¿Cuál es tu nombre? ")
 print(f"¡Hola, {nombre}!")
+if nombre == "Alice":
+    print("¡Eres la reina de las pruebas!")
+elif nombre == "Bob":
+    print("¡Eres el rey de las pruebas!")
+else:    print("¡Eres un tester común y corriente!")
